eb50b0b6e33c34515cc44106f948510f4a3957a4
Sven Liebig <liebigsv@gmail.com>
2017-02-22 17:09:42 +0100

    script files

---
+++ template/scripts/main.js
@@ -0,0 +1,10 @@
+//import Vote = require('../models/vote');
+"use strict";
+function init() {
+    return "null";
+}
+exports.init = init;
+function calculate(firstNum, secondNum, operation) {
+    return 1;
+}
+exports.calculate = calculate;
---
+++ template/scripts/main.ts
@@ -0,0 +1,9 @@
+//import Vote = require('../models/vote');
+
+export function init(): string {
+	return "null";
+}
+
+export function calculate(firstNum: number, secondNum: number, operation: string): number {
+	return 1;
+}
